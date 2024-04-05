4e8e094bbe4a6507bec1570ad3b6897d9a241b8e
Aloento <11802769+Aloento@users.noreply.github.com>
2024-04-06 00:05:00 +0200

    Refactor FeatureExtractionLayer module to include CBAMBlock and DepthwiseSeparableConvolution

--- FeatureExtractionLayer.py
+++ FeatureExtractionLayer.py
@@ -1,3 +1,5 @@
+from fightingcv_attention.attention.CBAM import CBAMBlock
+from fightingcv_attention.conv.DepthwiseSeparableConvolution import DepthwiseSeparableConvolution
 from torch import nn
 
 
@@ -74,21 +76,9 @@ class FEBlock2(nn.Module):
         self.in_channels = 16
         self.out_channels = 32
 
-        self.depthwise = nn.Conv2d(
-            in_channels=self.in_channels,
-            out_channels=self.in_channels,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            groups=self.in_channels
-        )
-
-        self.pointwise = nn.Conv2d(
-            in_channels=self.in_channels,
-            out_channels=self.out_channels,
-            kernel_size=1,
-            stride=1,
-            padding=0
+        self.depthwise = DepthwiseSeparableConvolution(
+            in_ch=self.in_channels,
+            out_ch=self.out_channels
         )
 
         self.bn = nn.BatchNorm2d(num_features=self.out_channels)
@@ -101,4 +91,57 @@ class FEBlock2(nn.Module):
         x = self.bn(x)
         x = self.lu(x)
         x = self.pool(x)
+        return x
+
+
+class FEBlock3(nn.Module):
+    """
+    1. Dilated Conv2d
+
+       ```python
+       in_channels: 32
+       out_channels: 64
+       kernel_size: 3
+       stride: 1
+       padding: 2
+       dilation: 2
+       ```
+
+    2. BatchNorm2d
+    3. PReLU
+    4. MaxPool2d
+
+       ```python
+       kernel_size: 2
+       stride: 2
+       ```
+
+    5. CBAM
+    """
+
+    def __init__(self):
+        super(FEBlock3, self).__init__()
+        self.in_channels = 32
+        self.out_channels = 64
+
+        self.conv = nn.Conv2d(
+            in_channels=self.in_channels,
+            out_channels=self.out_channels,
+            kernel_size=3,
+            stride=1,
+            padding=2,
+            dilation=2
+        )
+
+        self.bn = nn.BatchNorm2d(num_features=self.out_channels)
+        self.lu = nn.PReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.cbam = CBAMBlock(channel=self.out_channels, kernel_size=7)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.lu(x)
+        x = self.pool(x)
+        x = self.cbam(x)
         return x
