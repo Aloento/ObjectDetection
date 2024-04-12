038f0f5d2354dab2b0046979c974caa8a792cd53
Aloento <11802769+Aloento@users.noreply.github.com>
2024-04-12 10:49:00 +0200

    Refactor code to include Bottleneck module in FeatureLayer.py and remove ResBlock.py

---
+++ Bottleneck.py
@@ -0,0 +1,63 @@
+from torch import nn, Tensor
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 1, down_sample: nn.Module = None):
+        super(Bottleneck, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(num_features=out_channels)
+
+        self.conv2 = nn.Conv2d(
+            in_channels=out_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(num_features=out_channels)
+
+        self.conv3 = nn.Conv2d(
+            in_channels=out_channels,
+            out_channels=out_channels * self.expansion,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            bias=False
+        )
+        self.bn3 = nn.BatchNorm2d(num_features=out_channels * self.expansion)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.down_sample = down_sample
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x.clone()
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.down_sample is not None:
+            identity = self.down_sample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
--- FeatureLayer.py
+++ FeatureLayer.py
@@ -3,33 +3,28 @@ from torch import nn, Tensor
 from torch.nn import Sequential
 from torchvision.ops import DropBlock2d
 
-from ResBlock import ResBlock
+from Bottleneck import Bottleneck
 
 
 class FeatureLayer(nn.Module):
     def __init__(self):
         super(FeatureLayer, self).__init__()
-        self.in_channels = 64
+        self.in_planes = 64
 
         self.conv = nn.Conv2d(
             in_channels=3,
-            out_channels=self.in_channels,
+            out_channels=64,
             kernel_size=7,
             stride=2,
             padding=3,
             bias=False
         )
-        self.bn = nn.BatchNorm2d(self.in_channels)
-        self.lu = nn.PReLU(num_parameters=self.in_channels)
-
-        self.cbam = CBAMBlock(channel=self.in_channels, kernel_size=7)
+        self.bn = nn.BatchNorm2d(self.in_planes)
+        self.lu = nn.ReLU(inplace=True)
         self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.res_block1 = self.make_res_block(self.in_channels, 1)
+        self.res_block1 = self.make_res_block(64, 1)
         self.res_block2 = self.make_res_block(128, 2)
-
-        self.dropblock = DropBlock2d(block_size=3, p=0.3)
-
         self.res_block3 = self.make_res_block(256, 2)
         self.res_block4 = self.make_res_block(512, 2)
 
@@ -38,8 +33,8 @@ class FeatureLayer(nn.Module):
         layers = []
 
         for stride in strides:
-            layers.append(ResBlock(self.in_channels, out_channels, stride))
-            self.in_channels = out_channels
+            layers.append(Bottleneck(self.in_planes, out_channels, stride))
+            self.in_planes = out_channels
 
         return nn.Sequential(*layers)
 
@@ -47,14 +42,11 @@ class FeatureLayer(nn.Module):
         out = self.conv(x)
         out = self.bn(out)
         out = self.lu(out)
-
-        out = self.cbam(out)
         out = self.pool(out)
 
         out = self.res_block1(out)
         out = self.res_block2(out)
-        out = self.dropblock(out)
-
         out = self.res_block3(out)
         out = self.res_block4(out)
+
         return out
