4c6ba5d9ef9d810f90c141cc4b5ff19b6c5cdfc2
Aloento <11802769+Aloento@users.noreply.github.com>
2024-04-13 14:54:19 +0200

    Refactor code to include ResBlock module in FeatureLayer.py and remove Bottleneck.py

--- FeatureLayer.py
+++ FeatureLayer.py
@@ -2,31 +2,103 @@ import torch
 from torch import nn, Tensor
 from torch.nn import Sequential
 
-from Darknet import ConvBlock
+from Darknet import ConvBlock, ResBlock
 from VOCDataset import catalogs
 
 
 class FeatureLayer(nn.Module):
     def __init__(self):
         super(FeatureLayer, self).__init__()
-        self.input_seq = nn.Sequential(
-            ConvBlock(
-                in_channels=3,
-                out_channels=32,
-                kernel_size=3,
-                stride=1,
-                dcn=False
-            ),
+        self.input_conv = ConvBlock(
+            in_channels=3,
+            out_channels=32,
+            kernel_size=3,
+            stride=1,
+            dcn=False
+        )
+
+        self.x1_seq = Sequential(
             ConvBlock(
                 in_channels=32,
                 out_channels=64,
                 kernel_size=3,
                 stride=2,
-                dcn=True
-            )
+                dcn=False
+            ),
+            ResBlock(in_channels=64, out_channels=64)
         )
 
+        self.x2_seq = Sequential(
+            ConvBlock(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=3,
+                stride=2,
+                dcn=False
+            ),
+            ResBlock(in_channels=128, out_channels=128),
+            ResBlock(in_channels=128, out_channels=128)
+        )
 
-    def forward(self, x: Tensor) -> Tensor:
+        self.x8_seq1 = Sequential(
+            ConvBlock(
+                in_channels=128,
+                out_channels=256,
+                kernel_size=3,
+                stride=2,
+                dcn=False
+            ),
+            ResBlock(in_channels=256, out_channels=256),
+            ResBlock(in_channels=256, out_channels=256),
+            ResBlock(in_channels=256, out_channels=256),
+            ResBlock(in_channels=256, out_channels=256),
 
-        return out
+            ResBlock(in_channels=256, out_channels=256),
+            ResBlock(in_channels=256, out_channels=256),
+            ResBlock(in_channels=256, out_channels=256),
+            ResBlock(in_channels=256, out_channels=256)
+        )
+
+        self.x8_seq2 = Sequential(
+            ConvBlock(
+                in_channels=256,
+                out_channels=512,
+                kernel_size=3,
+                stride=2,
+                dcn=False
+            ),
+            ResBlock(in_channels=512, out_channels=512),
+            ResBlock(in_channels=512, out_channels=512),
+            ResBlock(in_channels=512, out_channels=512),
+            ResBlock(in_channels=512, out_channels=512),
+
+            ResBlock(in_channels=512, out_channels=512),
+            ResBlock(in_channels=512, out_channels=512),
+            ResBlock(in_channels=512, out_channels=512),
+            ResBlock(in_channels=512, out_channels=512)
+        )
+
+        self.x4_seq = Sequential(
+            ConvBlock(
+                in_channels=512,
+                out_channels=1024,
+                kernel_size=3,
+                stride=2,
+                dcn=False
+            ),
+            ResBlock(in_channels=1024, out_channels=1024),
+            ResBlock(in_channels=1024, out_channels=1024),
+            ResBlock(in_channels=1024, out_channels=1024),
+            ResBlock(in_channels=1024, out_channels=1024)
+        )
+
+    def forward(self, x: Tensor) -> (Tensor, Tensor, Tensor):
+        inp = self.input_conv(x)
+
+        x1 = self.x1_seq(inp)
+        x2 = self.x2_seq(x1)
+        x8_1 = self.x8_seq1(x2)
+        x8_2 = self.x8_seq2(x8_1)
+        x4 = self.x4_seq(x8_2)
+
+        return x8_1, x8_2, x4
